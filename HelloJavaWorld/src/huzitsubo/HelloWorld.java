f7c5a6318b2ed5d80980d60118597654e874d84f
Huzitsubo <hshs63prpr@gmail.com>
2016-07-14 10:38:44 +0900

    初めてのコミット

---
+++ HelloJavaWorld/src/huzitsubo/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package huzitsubo;
+
+/**
+ * @author Huzitsubo
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hoge");
+	}
+
+}
